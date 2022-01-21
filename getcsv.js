c40e0950ad812c15d26409e4ac5c2657c2830d1a
davidaddi <94124302+davidaddi@users.noreply.github.com>
2022-01-21 08:56:45 +0100

    Update getcsv.js

--- getcsv.js
+++ getcsv.js
@@ -1,57 +1,74 @@
-require('isomorphic-fetch')
-const converterCsvToJson = require("csvtojson");
+// package (https://www.npmjs.com/package/isomorphic-fetch)
+// - provides an implementation of the fetch API compatible with Nodejs
+// $ npm install isomorphic-fetch
+
+const fetch = require('isomorphic-fetch');
+
+// package (https://nodejs.org/api/fs.html)
+// - provides a utility for manipulating system files
+// npm install fs
 const fs = require('fs');
+
+// package (https://nodejs.org/api/path.html)
+// - fournit: un utilitaire de manipulation des chemins systèmes
+// npm install --save path
+
 const path = require('path');
+
+// created a folder to store the downloaded files
 const PATH_FOLDER_DOWNLOAD = path.join(__dirname, "./files");
- 
+
 if(!fs.existsSync(PATH_FOLDER_DOWNLOAD)) {
+    // if the download folder does not exist we create it
     fs.mkdirSync(PATH_FOLDER_DOWNLOAD);
 }
- 
-const TIME_DOWNLOAD_INTERVAL = ((1_000 * 60) * 60) * 24; // 24 hours
-const downloadRid = setInterval(onDownload, TIME_DOWNLOAD_INTERVAL);
- 
+
 async function onDownload() {
- 
+    // get the link of the last CSV file
     const response = await fetch('https://www.data.gouv.fr/api/2/datasets/6010206e7aa742eb447930f7/resources/?page=1&type=main&page_size=1', {
         method: "GET"
     });
- 
+
     const data = await response.json();
     // console.log(data)
 
-    const lastItem = data => {
-        data.data[0]
-    }
+    const lastItem = data.data[0];
 
-    const integrity = lastItem => {
-        lastItem.checksum.value;
-    }
+    // check if the file is not already owned, using a hash to uniquely identify the data
+    const integrity = 'data';
 
-    const filenames = fs  => {
-        fs.readdirSync(PATH_FOLDER_DOWNLOAD, {encoding: "utf-8"});
-    }
+    // Using simple `fs.readdirSync` would give you filenames as well as directories.
+    // This approach creates objects called `Dirent`s where you can check if the the entry is a file or a directory.
+    // Then you can simply filter out the directories and get the filenames.
+    const filenames = fs.readdirSync(PATH_FOLDER_DOWNLOAD, {withFileTypes: true, encoding: "utf-8"})
+        .filter(dirent => dirent.isFile())
+        .map(dirent => dirent.name);
 
-    const exists = filenames => {
-        find.filename.indexOf(integrity) !== -1;
-    }
- 
+    // Check if some of the filenames contains the integrity hash
+    const exists = filenames.some(filename => filename.indexOf(integrity) !== -1);
+
+    // we already have this file it has not been updated yet we give up the operation
     if(exists) {
         const {published} = lastItem;
-        console.log(`opération annulée, aucun fichier à été publié depuis le: ${published}`);
- 
+        console.log(`operation cancelled, no file has been published since: ${published}`);
+
     } else {
+        // we don't own this file we download it
         const urlCsvFile = lastItem.url;
         const response = await fetch(urlCsvFile, {method: "GET"});
         const csvContent = await response.text();
- 
+
+        // writes the new file to the download folder
+        // using the hash as the file name, which will allow later to check if you already have it
+
         fs.writeFileSync(
             path.join(PATH_FOLDER_DOWNLOAD, (integrity + ".csv")),
             csvContent,
             {encoding: "utf-8"}
-        ); 
-        const json = await converterCsvToJson().fromString(csvContent);
-        console.log(json);
+        );
+
+        // the CSV file has been downloaded and saved in a file at: /download/{hash}.csv
+        console.log(' ✅ File downloaded!')
     }
 }
 
